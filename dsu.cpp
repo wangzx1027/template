ebd3281cf2d628a8306be327960e6c0b949aced8
hitWang <386323419@qq.com>
2026-05-17 16:00:32 +0800

    update dsu

--- dsu.cpp
+++ dsu.cpp
@@ -1,17 +1,16 @@
 class dsu {
  public:
   vector<int> p;
-  int n;
- 
+  int n; 
   dsu(int _n) : n(_n) {
     p.resize(n);
     iota(p.begin(), p.end(), 0);
   }
- 
+
   inline int get(int x) {
     return (x == p[x] ? x : (p[x] = get(p[x])));
   }
- 
+
   inline bool unite(int x, int y) {
     x = get(x);
     y = get(y);
